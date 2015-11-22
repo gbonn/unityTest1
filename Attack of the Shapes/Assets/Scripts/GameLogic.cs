5e0006dd184cdd0a1bd4417d2680ae8de8c4b906
gbonn <greg.bonn@gmail.com>
2015-11-22 11:49:13 -0700

    Updates for build
    Minor change to vertical walls. Built executable for Windows.

--- Attack of the Shapes/Assets/Scripts/GameLogic.cs
+++ Attack of the Shapes/Assets/Scripts/GameLogic.cs
@@ -16,7 +16,11 @@ public class GameLogic : MonoBehaviour {
 		// the WYSIWIG editor, but I wanted to see how the Instantiate call worked...
 		// The numbers here are all based on the size of the vertical border sprite.
 		for (int numVertBorderElements = 0; numVertBorderElements < 4; numVertBorderElements++) {
-			Instantiate (borderPrefab, new Vector3 (-2, numVertBorderElements * 4, 0), Quaternion.identity);
+			// TODO: This is a hack to make a blank space for the UI elements to sit.
+			// Needs better UI...
+			if (numVertBorderElements < 3) {
+				Instantiate (borderPrefab, new Vector3 (-2, numVertBorderElements * 4, 0), Quaternion.identity);
+			}
 			Instantiate (borderPrefab, new Vector3 (12, numVertBorderElements * 4, 0), Quaternion.identity);
 		}
 	}
